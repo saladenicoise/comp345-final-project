8faed999c7753e47671aae0811f345f7f0712a9b
kkaylee7 <85369041+kkaylee7@users.noreply.github.com>
2021-10-06 19:31:53 -0400

    Add files via upload

---
+++ Player/Player.h
@@ -0,0 +1,78 @@
+//This is the header file Player.h. 
+//It contains all the class and operator declarations.
+//Also provides dummy classes for Territory class, Card class and Order class.
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+#include <vector>
+#include <iostream>
+#include <ostream> 
+
+using namespace std;
+
+class Player;
+class Territory;
+class Cards;
+class Order;
+
+class Player //this is the Player class
+{
+    
+    public:
+        Player(); //default constructor
+        Player(string playerName, int pid,Territory* t,Cards* c, vector<Order*>); //constructor  
+        Player(const Player& p); //copy constructor
+        virtual ~Player(); //deconstructor 
+        virtual string getPlayerName();
+        virtual int getPID(); 
+        void toAttack();
+        void toDefend();
+        void issueOrder(string);
+        void printOrder();
+        vector<Order*> getOrderList();
+        virtual Player& operator=(const Player& p); //overloaded assignment operator
+        friend std::ostream& operator<<(std::ostream &os,const Player& p); //stream insertion operator
+
+    private:
+        string playerName;
+        int pid;
+        Territory* t;
+        Cards* c; 
+        vector<Order*> orderList;
+};
+
+class Territory //dummy class for Territory 
+{
+    public:
+        Territory(); //default constructor 
+        void setTerritory(string name);     //mutator method
+        void getTerritory();    //receives territories 
+
+    private:
+        string name; 
+        vector<string*> t;
+};
+
+class Cards //dummy class for Cards
+{
+    public:
+        Cards(); //default constructor
+        void setCard(string card); //mutator method for cards
+        void hands();   //receives card hands
+
+    private:
+        string card;
+        vector<string*> c;
+};
+
+class Order //dummy class for Order
+{
+    public:
+        Order(); //default constructor
+        Order(string order); //constructor
+        string returnOrder();  //returns order
+
+    private:
+        string order;
+};
+#endif //PLAYER_H
---
+++ Player/PlayerDriver.cpp
@@ -0,0 +1,24 @@
+#include "Player.h"
+
+int main()
+{
+    string name = "John";
+    Territory* t = new Territory();
+    t->setTerritory("Europe");
+    t->setTerritory("Russia");
+    Cards* c = new Cards();
+    c->setCard("bomb");
+    c->setCard("airlift");
+    vector<Order*> o;
+
+    Player* player = new Player(name,1,t,c,o);
+    player->toAttack();
+    player->issueOrder("bomb");
+    player->issueOrder("deploy");
+    player->printOrder();
+    Player* player2 = new Player(*player);
+    player2->toDefend();
+    Player* player3;
+    
+    return 0;
+}
