b18b4d72c9d0355cde63aca11cdd0dd472c233b0
mite-pat <90350918+mite-pat@users.noreply.github.com>
2021-11-12 23:06:41 -0500

    added comments

--- Command/CommandProcessing.h
+++ Command/CommandProcessing.h
@@ -9,56 +9,56 @@ using namespace std;
 class Command : public ILoggable, public Subject {
 
 private:
-    string command;
-    string effect;
+    string command; // variable to store the command
+    string effect; // variable to store the effect
 public:
-    Command();
-    Command(Command& comObj);
-    Command(string command);
-    ~Command();
-    void setCommand(string command);
-    string getCommand();
-    void saveEffect(string effect);
-    string getEffect();
-    std::string stringToLog();
+    Command(); // default constructor
+    Command(Command& comObj); // copy constructor
+    Command(string command); // single parameter constructor
+    ~Command(); // destructor
+    void setCommand(string command); // mutator for command
+    string getCommand(); // accessor for command
+    void saveEffect(string effect); // mutator for effect
+    string getEffect(); // accessor for effect
+    std::string stringToLog(); // string to log function
 
 };
 
 class CommandProcessor : public ILoggable, public Subject {
 
 private:
-    virtual string readCommand();
-    void saveCommand(string command);
+    virtual string readCommand(); // read command function
+    void saveCommand(string command); // save command to command object
 public:
-    vector<Command*> commands;
-    CommandProcessor();
-    CommandProcessor(CommandProcessor& comProcObj);
-    ~CommandProcessor();
-    void getCommand();
-    bool checkIfValidCommand(string command);
-    bool validate(string command, vector<string> nextValidCommands);
-    std::string stringToLog();
+    vector<Command*> commands; // vector of commands
+    CommandProcessor(); // default constructor
+    CommandProcessor(CommandProcessor& comProcObj); // copy constructor
+    ~CommandProcessor(); // destructor
+    void getCommand(); // accessor for commandprocessor
+    bool checkIfValidCommand(string command); // check if valid command
+    bool validate(string command, vector<string> nextValidCommands); // validate method for next viable command
+    std::string stringToLog(); // string to log function
 
 };
 
 class FileLineReader {
 private:
-    string filename;
-    fstream myFile;
+    string filename; // filename var
+    fstream myFile; // stream to open file
 public:
-    FileLineReader(string filename);
-    FileLineReader(const FileLineReader& flr);
-    ~FileLineReader();
-    string readLineFromFile();
+    FileLineReader(string filename); // single parameterized constructor
+    FileLineReader(const FileLineReader& flr); // copy constructor
+    ~FileLineReader(); // destructor
+    string readLineFromFile(); // function to read file
 };
 
 class FileCommandProcessorAdapter : public CommandProcessor {
 private:
-    FileLineReader* flr;
-    virtual string readCommand() override;
+    FileLineReader* flr; // file reader object
+    virtual string readCommand() override; // override readcommand function
 public:
-    FileCommandProcessorAdapter(FileLineReader flr);
-    ~FileCommandProcessorAdapter();
+    FileCommandProcessorAdapter(FileLineReader flr); // single parameterized constructor
+    ~FileCommandProcessorAdapter(); // destructor
 
 
 
